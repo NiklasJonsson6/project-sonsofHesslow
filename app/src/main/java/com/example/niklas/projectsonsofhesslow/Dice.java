e2d18785ae7811c356c4dad109574700b36d8c2f
Arvid <hasta@student.chalmers.se>
2016-04-12 18:12:00 +0200

    added Dice class

--- app/src/main/java/com/example/niklas/projectsonsofhesslow/Dice.java
+++ app/src/main/java/com/example/niklas/projectsonsofhesslow/Dice.java
@@ -1,7 +1,15 @@
 package com.example.niklas.projectsonsofhesslow;
+import java.util.Random;
 
-/**
- * Created by Arvid on 12/04/2016.
- */
 public class Dice {
+    public int roll(int xCordinate, int yCordinate){
+        Random random = new Random();
+        int number = random.nextInt(6) + 1;
+        playAnimation(number, xCordinate, yCordinate);
+        return number;
+    }
+
+    private void playAnimation(int number, int xCordinate, int yCordinate){
+        //code for animation at cordinates x, y
+    }
 }
