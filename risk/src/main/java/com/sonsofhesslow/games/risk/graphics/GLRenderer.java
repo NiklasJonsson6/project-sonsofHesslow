b28c63702e6a06fd9aa20b0d6026d718fafbe8a1
Daniel Hesslow <Daniel.hesslow@gmail.com>
2016-05-29 19:47:04 +0200

    background color

--- risk/src/main/java/com/sonsofhesslow/games/risk/graphics/GLRenderer.java
+++ risk/src/main/java/com/sonsofhesslow/games/risk/graphics/GLRenderer.java
@@ -120,7 +120,7 @@ public class GLRenderer implements GLSurfaceView.Renderer, Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
-        GLES20.glClearColor(1f, 1f, 1f, 1.0f);
+        GLES20.glClearColor(0.8f, 1f, 1f, 1.0f);
         GLES20.glEnable(GLES20.GL_BLEND);
         GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
     }
